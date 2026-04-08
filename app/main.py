1cc95cd9f17943340586865210e01339739ac1c1
Sumit Das <sumitdas20111984@gmail.com.com>
2026-04-09 05:28:24 +0530

    feat: Add dotenv support and update upload timeout

--- app/main.py
+++ app/main.py
@@ -1,3 +1,7 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
 from fastapi import FastAPI
 
 from app.models.database import engine
--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from finaudit!")
-
-
-if __name__ == "__main__":
-    main()
